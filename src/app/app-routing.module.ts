227eb23a1eb111ef8f7e046e66c443ae63d1bdde
Jeremiah Teague <jeremiah.teague@suez.com>
2019-03-22 15:04:32 -0600

    making it my own

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SongDetailComponent } from './song-detail/song-detail.component';
 
 const routes: Routes = [
-	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: '', redirectTo: '/playlist', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'playlist', component: PlaylistComponent },
 	{ path: 'detail/:id', component: SongDetailComponent },
--- src/app/song.ts
+++ src/app/song.ts
@@ -2,4 +2,8 @@ export class Song {
 	id: number;
 	name: string;
 	notes: string;
+	badHorns: bool;
+	badRhythm: bool;
+	badStart: bool;
+	badEnd: bool;
 }
